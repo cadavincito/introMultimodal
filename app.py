67939a2c3b2b0e3a2192930ae33242c9c11b3799
Cadavinci <cadavincinfo@gmail.com>
2025-02-27 07:32:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,22 @@ texto = st.text_input("escribe algo","este es mi texto")
 
 st.write("el texto escrito es",texto)
 
+with col1:
+  st.subheader("columna 1")
+  st.write("las interfaces multimodales mejoran la UX")
+  resp = st.checkbox("de acuerdo")
+  if resp:
+    st.write("correcto")
+
+with col2:
+  st.subheader("columna 1")
+  modo = st.radio("que modalidad prefieres", ("visual","auditiva","tactil"))
+  if modo == "visual":
+    st.write("la vista es fundamental para la ui")
+  if modo == "auditiva":
+    st.write("la audicion es fundamental para la ui")
+  if modo == "tactil":
+    st.write("el tacto es fundamental para la ui")
+  
+  
+  
